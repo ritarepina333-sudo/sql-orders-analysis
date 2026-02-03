6e8b5a1b8ecf2d94f95b30e839505c2ab67c6d75
ritarepina333-sudo <ritarepina333@gmail.com>
2026-02-03 21:32:54 +0300

    Update queries.sql

--- queries.sql
+++ queries.sql
@@ -1,10 +1,23 @@
+-- Общее количество всех заказов
 SELECT COUNT(*) AS total_orders
 FROM orders;
 
+-- Среднее значение среди всех цен
 SELECT AVG(price) AS avg_order_price
 FROM orders;
 
+-- Минимальное и максимальное значнеие по цене заказов
 SELECT 
     MIN(price) AS min_order_price,
     MAX(price) AS max_order_price
 FROM orders;
+
+-- Количество заказов по статусам (с указанным статусом)
+SELECT status, COUNT(*) AS count_order 
+FROM orders
+GROUP BY status;
+
+-- Средний чек по статусам (с указанным статусом)
+SELECT status, AVG(price) AS average_price 
+FROM orders
+GROUP BY status;
